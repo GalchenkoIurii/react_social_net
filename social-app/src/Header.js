9427361bd9fd08f277f5364a811327340dad2f55
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-03-11 07:39:45 +0200

    put components in different files

---
+++ social-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+        <ul>
+        <li><a href="#">Home</a></li>
+    <li><a href="#">Blog</a></li>
+    <li><a href="#">Contacts</a></li>
+    </ul>
+    </div>
+);
+}
+
+export default Header;
