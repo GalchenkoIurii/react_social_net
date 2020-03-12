902f4d6fe04789badf5e7bdccea8050c31723314
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-03-12 07:37:16 +0200

    main markup and styles

--- social-app/src/modules/Header.jsx
+++ social-app/src/modules/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>
-            
+        <header className="header">
+            <img src="https://www.johnsoncontrols.com/-/media/jci/be/united-states/our-brands/720/fx.png?h=320&w=720&la=en&hash=0B1939430AB3A509897A620A0B1147A89E287546" alt=""/>
         </header>
     );
 }
+
+export default Header;
