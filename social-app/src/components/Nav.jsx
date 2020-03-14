361c979d890422e84d67b3048721984ba8cdbb1a
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-03-14 21:54:27 +0200

    split styles to css-modules

--- social-app/src/components/Nav.jsx
+++ social-app/src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Nav.css';
+import classes from './Nav.module.css';
 
 const Nav = () => {
     return (
-        <nav className="nav">
+        <nav className={classes.nav}>
             <ul>
-                <li className="item"><a>Profile</a></li>
+                <li className={`${classes.item} ${classes.link}`}><a>Profile</a></li>
                 <li className="item"><a>Messages</a></li>
                 <li className="item"><a>Posts</a></li>
                 <li className="item"><a>News</a></li>
--- social-app/src/components/Profile.jsx
+++ social-app/src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Profile.css';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <section className="content">
-            <div className="main-img">
+        <section className={classes.content}>
+            <div className={classes.mainImg}>
                 <img src="https://community.kodular.io/uploads/default/original/2X/6/6218d50608595a107c07c35a12979966a620d433.jpg" alt=""/>
             </div>
             <h2>Content</h2>
