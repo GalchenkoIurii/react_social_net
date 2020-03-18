8f32e073ad3c8e93a51650a5937401fef713205a
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-03-18 21:54:49 +0200

    added NavLink to menu items

--- social-app/src/components/Nav/Nav.jsx
+++ social-app/src/components/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className={classes.nav}>
             <ul>
-                <li className={`${classes.item} ${classes.link}`}><a href="/profile">Profile</a></li>
-                <li className="item"><a href="/messages">Messages</a></li>
-                <li className="item"><a href="/posts">Posts</a></li>
-                <li className="item"><a href="/news">News</a></li>
+                <li className={`${classes.item} ${classes.link}`}><NavLink to="/profile">Profile</NavLink></li>
+                <li className="item"><NavLink to="/messages">Messages</NavLink></li>
+                <li className="item"><NavLink to="/posts">Posts</NavLink></li>
+                <li className="item"><NavLink to="/news">News</NavLink></li>
             </ul>
         </nav>
     );
