ab087db7f1d87ee3112228d8061474faf78cdab8
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-03-20 07:15:23 +0200

    added some styles

--- social-app/src/components/Header/Header.jsx
+++ social-app/src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import classes from './Header.module.css';
 const Header = () => {
     return (
         <header className={classes.header}>
-            <img src="https://www.johnsoncontrols.com/-/media/jci/be/united-states/our-brands/720/fx.png?h=320&w=720&la=en&hash=0B1939430AB3A509897A620A0B1147A89E287546" alt=""/>
+            <div className={classes.container}>
+                <img src="https://www.johnsoncontrols.com/-/media/jci/be/united-states/our-brands/720/fx.png?h=320&w=720&la=en&hash=0B1939430AB3A509897A620A0B1147A89E287546" alt=""/>
+            </div>
         </header>
     );
 }
