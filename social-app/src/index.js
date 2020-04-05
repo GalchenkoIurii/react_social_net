556f549608b07cf7a138f457e9623835dd853b84
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-04-05 19:39:21 +0300

    changed index.js and state.js

--- social-app/src/index.js
+++ social-app/src/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
-
+import store from "./redux/state";
 import App from './App';
-import {addMessage, addPost, updateNewMessageData, updateNewPostData} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 let render = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostData={updateNewPostData}
-                 addMessage={addMessage} updateNewMessageData={updateNewMessageData} />
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostData={store.updateNewPostData.bind(store)}
+                 addMessage={store.addMessage.bind(store)} updateNewMessageData={store.updateNewMessageData.bind(store)} />
         </BrowserRouter>,
         document.getElementById('root'));
 };
 
-render(state);
+render(store.getState());
 
-subscribe(render);
+store.subscribe(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- social-app/src/redux/state.js
+++ social-app/src/redux/state.js
@@ -1,74 +1,150 @@
-let render = () => {
-    console.log('state changed');
+let store = {
+    _state: {
+        usersData: [
+            {id: "john", name: "John", avatarUrl: "https://www.arbse.net/Files/Warcraft3_tft/id149/warcraft_3_solution.jpg"},
+            {id: "jack", name: "Jack", avatarUrl: "https://i.ytimg.com/vi/WxUeT4sfEB8/hqdefault.jpg"},
+            {id: "james", name: "James", avatarUrl: "https://image.shutterstock.com/image-illustration/3d-render-abstract-face-analysis-260nw-1527977660.jpg"}
+        ],
+        messagesData: [
+            {id: 1, message: "First message"},
+            {id: 2, message: "One more message"},
+            {id: 3, message: "And one more message"}
+        ],
+        newMessageData: "",
+        postsData: [
+            {id: 1, title: "First post", postContent: "First post content"},
+            {id: 2, title: "Second post", postContent: "Second post content"},
+            {id: 3, title: "Third post", postContent: "Third post content"}
+        ],
+        newPostData: "",
+        contactsData: [
+            {id: 1, name: "Genry", avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2EWiPWkXNn2VBN_RdN0nJ2Rn5huij1ovZQZmetyG03Armpnf2"},
+            {id: 2, name: "Andrew", avatarUrl: "https://cdn.igromania.ru/mnt/news/d/6/e/d/a/d/87586/e0d6d89899ada4cf_1920xH.jpg"}
+        ]
+    },
+    _callSubscriber() {
+        console.log('state changed');
+    },
+    getState() {
+        return this._state;
+    },
+    addMessage() {
+        let newMessage = {
+            id: 4,
+            message: this._state.newMessageData
+        };
+
+        this._state.messagesData.push(newMessage);
+
+        this._state.newMessageData = "";
+
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageData(newMessageText) {
+
+        this._state.newMessageData = newMessageText;
+
+        this._callSubscriber(this._state);
+    },
+    addPost() {
+        let newPost = {
+            id: 4,
+            title: "Fourth post",
+            postContent: this._state.newPostData
+        };
+
+        this._state.postsData.push(newPost);
+
+        this._state.newPostData = "";
+
+        this._callSubscriber(this._state);
+    },
+    updateNewPostData(newPostText) {
+
+        this._state.newPostData = newPostText;
+
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }
 };
 
-let state = {
-    usersData: [
-        {id: "john", name: "John", avatarUrl: "https://www.arbse.net/Files/Warcraft3_tft/id149/warcraft_3_solution.jpg"},
-        {id: "jack", name: "Jack", avatarUrl: "https://i.ytimg.com/vi/WxUeT4sfEB8/hqdefault.jpg"},
-        {id: "james", name: "James", avatarUrl: "https://image.shutterstock.com/image-illustration/3d-render-abstract-face-analysis-260nw-1527977660.jpg"}
-    ],
-    messagesData: [
-        {id: 1, message: "First message"},
-        {id: 2, message: "One more message"},
-        {id: 3, message: "And one more message"}
-    ],
-    newMessageData: "",
-    postsData: [
-        {id: 1, title: "First post", postContent: "First post content"},
-        {id: 2, title: "Second post", postContent: "Second post content"},
-        {id: 3, title: "Third post", postContent: "Third post content"}
-    ],
-    newPostData: "",
-    contactsData: [
-        {id: 1, name: "Genry", avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2EWiPWkXNn2VBN_RdN0nJ2Rn5huij1ovZQZmetyG03Armpnf2"},
-        {id: 2, name: "Andrew", avatarUrl: "https://cdn.igromania.ru/mnt/news/d/6/e/d/a/d/87586/e0d6d89899ada4cf_1920xH.jpg"}
-    ]
-};
+export default store;
 
-export const addMessage = () => {
-    let newMessage = {
-        id: 4,
-        message: state.newMessageData
-    };
+window.store = store;
 
-    state.messagesData.push(newMessage);
+// let render = () => {
+//     console.log('state changed');
+// };
 
-    state.newMessageData = "";
+// let state = {
+//     usersData: [
+//         {id: "john", name: "John", avatarUrl: "https://www.arbse.net/Files/Warcraft3_tft/id149/warcraft_3_solution.jpg"},
+//         {id: "jack", name: "Jack", avatarUrl: "https://i.ytimg.com/vi/WxUeT4sfEB8/hqdefault.jpg"},
+//         {id: "james", name: "James", avatarUrl: "https://image.shutterstock.com/image-illustration/3d-render-abstract-face-analysis-260nw-1527977660.jpg"}
+//     ],
+//     messagesData: [
+//         {id: 1, message: "First message"},
+//         {id: 2, message: "One more message"},
+//         {id: 3, message: "And one more message"}
+//     ],
+//     newMessageData: "",
+//     postsData: [
+//         {id: 1, title: "First post", postContent: "First post content"},
+//         {id: 2, title: "Second post", postContent: "Second post content"},
+//         {id: 3, title: "Third post", postContent: "Third post content"}
+//     ],
+//     newPostData: "",
+//     contactsData: [
+//         {id: 1, name: "Genry", avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2EWiPWkXNn2VBN_RdN0nJ2Rn5huij1ovZQZmetyG03Armpnf2"},
+//         {id: 2, name: "Andrew", avatarUrl: "https://cdn.igromania.ru/mnt/news/d/6/e/d/a/d/87586/e0d6d89899ada4cf_1920xH.jpg"}
+//     ]
+// };
 
-    render(state);
-};
+// export const addMessage = () => {
+//     let newMessage = {
+//         id: 4,
+//         message: state.newMessageData
+//     };
+//
+//     state.messagesData.push(newMessage);
+//
+//     state.newMessageData = "";
+//
+//     render(state);
+// };
 
-export const updateNewMessageData = (newMessageText) => {
+// export const updateNewMessageData = (newMessageText) => {
+//
+//     state.newMessageData = newMessageText;
+//
+//     render(state);
+// };
 
-    state.newMessageData = newMessageText;
+// export const addPost = () => {
+//     let newPost = {
+//         id: 4,
+//         title: "Fourth post",
+//         postContent: state.newPostData
+//     };
+//
+//     state.postsData.push(newPost);
+//
+//     state.newPostData = "";
+//
+//     render(state);
+// };
 
-    render(state);
-};
+// export const updateNewPostData = (newPostText) => {
+//
+//     state.newPostData = newPostText;
+//
+//     render(state);
+// };
 
-export const addPost = () => {
-    let newPost = {
-        id: 4,
-        title: "Fourth post",
-        postContent: state.newPostData
-    };
+// export const subscribe = (observer) => {
+//     render = observer;
+// };
 
-    state.postsData.push(newPost);
-
-    state.newPostData = "";
-
-    render(state);
-};
-
-export const updateNewPostData = (newPostText) => {
-
-    state.newPostData = newPostText;
-
-    render(state);
-};
-
-export const subscribe = (observer) => {
-    render = observer;
-};
-
-export default state;
+// export default state;
