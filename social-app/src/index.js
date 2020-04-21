7b7f6941b6376461bceab543369b5dccad085b1c
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-04-21 11:52:17 +0300

    added StoreContext

--- social-app/src/index.js
+++ social-app/src/index.js
@@ -5,11 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import store from "./redux/redux-store";
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import StoreContext from "./StoreContext";
 
 let render = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            <StoreContext.Provider value={store}>
+                <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            </StoreContext.Provider>
         </BrowserRouter>,
         document.getElementById('root'));
 };
